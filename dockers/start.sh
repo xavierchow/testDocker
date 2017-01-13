47edb69542d58414b8fb276393a32911a8d881b5
Xavier Zhou <xiayezhou@gmail.com>
2017-01-13 14:20:00 +0800

    Add port sniff

--- dockers/start.sh
+++ dockers/start.sh
@@ -5,6 +5,32 @@ pushd `dirname $0`
 docker-compose up -d  --build
 docker-compose ps
 popd
+
+function try()
+{
+    [[ $- = *e* ]]; SAVED_OPT_E=$?
+    set +e
+}
+
+function catch()
+{
+    export ex_code=$?
+    (( $SAVED_OPT_E )) && set +e
+    return $ex_code
+}
+
+while true; do
+  try
+  (
+    curl -X POST http://localhost:13003/ && curl -X POST http://localhost:5000/
+  )
+  catch || {
+    sleep 3
+    continue
+  }
+  break
+done
+
 netstat -an | grep 003
 ifconfig
 
