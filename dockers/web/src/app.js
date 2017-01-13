b65ddbdcf095d112dd6c03f803cc221f92598e8e
Xavier Zhou <xiayezhou@gmail.com>
2017-01-13 10:50:19 +0800

    Add dependency servcie

---
+++ dockers/web/src/app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express')
+var app = express()
+
+app.post('/web/context/:txnId', function (req, res) {
+
+  const result = {
+    data: { id: req.params.txnId }
+  };
+  res.json(result);
+})
+
+app.post('/web/action/:txnId', function (req, res) {
+
+  const result = {
+    data: { id: req.params.txnId }
+  };
+  res.json(result);
+})
+
+
+app.listen(3003, () => {
+  console.log('Mock Web listening on port 3003');
+})
+
